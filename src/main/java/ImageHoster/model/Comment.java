3d8b98fa5ac4f4d82b993fc0e97f3393398a54f2
aahanalaksh <53683765+aahanalaksh@users.noreply.github.com>
2019-09-29 22:36:49 +0530

    Comment Model

--- src/main/java/ImageHoster/model/Comment.java
+++ src/main/java/ImageHoster/model/Comment.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Comment Model which has a relationship with image and the user
  */
 package ImageHoster.model;
 
